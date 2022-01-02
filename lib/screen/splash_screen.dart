8b6261716d0e690c3cf3ec9f45c3a60182a83f2c
donghun <hoonyhoeny@gmail.com>
2022-01-02 21:51:54 +0900

    splash screen에 lottie 추가

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -2,54 +2,50 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:lotto_maker_flutter/screen/homeView.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+  with SingleTickerProviderStateMixin {
+
+  late AnimationController controller;
 
   @override
   void initState() {
     super.initState();
-    Timer(
-      Duration(seconds: 4),
-          () => Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => MyHomePage()),
-      ),
-    );
-  }
 
+    controller= AnimationController(
+      duration: Duration(seconds: 3),
+        vsync:this,
+    );
+
+  }
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/splash.gif'), fit: BoxFit.contain),
-      ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0.0,
-            left: 0.0,
-            child: Container(
-              width: width,
-              height: height,
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                body: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Center(),
-                ),
-              ),
-            ),
+    return  Scaffold(
+      body: Center(
+        child:
+          // Load a Lottie file from your assets
+          Lottie.asset('assets/lottery.json',
+          controller: controller,
+            animate: true,
+            onLoaded: (composition) {
+              controller
+                ..duration = composition.duration
+                ..forward().whenComplete(() => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                ));
+            },
           ),
-        ],
+
       ),
     );
   }
