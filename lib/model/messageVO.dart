8e46108be9dcf246ec8931976405d8dc280dd3a4
donghun <hoonyhoeny@gmail.com>
2022-03-09 22:17:55 +0900

    무한스크롤 기능 수정

--- lib/model/messageVO.dart
+++ lib/model/messageVO.dart
@@ -1,12 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class MessageVO {
-    String docId;
-    String messageText;
-    String time;
-    String timesAgo;
-    int likey;
+  String docId;
+  String messageText;
+  String time;
+  String timesAgo;
+  int likey;
 
-    MessageVO({required this.docId, required this.messageText, required this.time,required this.timesAgo,required this.likey});
+  MessageVO(
+      {required this.docId, required this.messageText, required this.time, required this.timesAgo, required this.likey});
+
+  MessageVO.fromJson(Map<String, dynamic> json)
+      : docId = json['docId'],
+        messageText = json['messageText'],
+        time = json['time'],
+        timesAgo = json['timesAgo'],
+        likey = json['likey'];
+
+  Map<String, dynamic> toJson() => {
+    'docId': docId,
+    'messageText': messageText,
+    'time': time,
+    'timesAgo': timesAgo,
+    'likey': likey,
+  };
 
 }
--- lib/subViews/reply.dart
+++ lib/subViews/reply.dart
@@ -19,177 +19,180 @@ class _ReplyScreenState extends State<ReplyScreen> {
   String inputText = '';
   int likey = 0;
   DateTime formatDate = DateTime.now().toLocal(); //format변경
-  List<dynamic> messageList = []; //서버데이타를 담은것(Snapshot에서 string 추출)
+  bool loading =false, allLoaded =false ;
+  List<MessageVO> messageList = [];
+  MessageVO msgVO= new MessageVO(docId: '', messageText: '', time: '', timesAgo: '', likey: 0);
 
+  @override
+  void initState() {
+    mockFetch();
+  }
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         child: Column(
           children: [
-            Row(
-              children: [
-                SizedBox(
-                  width: 50.0,
-                  height: 50.0,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://randomuser.me/api/portraits/men/28.jpg"),
+        Row(
+        children: [
+        SizedBox(
+        width: 50.0,
+          height: 50.0,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://randomuser.me/api/portraits/men/28.jpg"),
+          ),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        Container(
+          width: MediaQuery
+              .of(context)
+              .size
+              .width * 0.7,
+          padding: EdgeInsets.only(left: 5.0, top: 5.0),
+          height: 30.0,
+          decoration: BoxDecoration(
+            color: Colors.grey[350],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextField(
+                  //textInputAction: TextInputAction.go, //엔터키 치면 제출되게 설정
+                  controller: messageTextController,
+                  onSubmitted: (value) {
+                    inputText = value;
+                    messageTextController.clear();
+                    _firestore.collection('post').add({
+                      //post라는 컬렉션에 contents컬럼으로 입력
+                      'contents': inputText,
+                      'time': DateFormat('yyyy-MM-dd kk:mm:ss')
+                          .format(DateTime.now().toLocal()),
+                      'likey': likey,
+                    });
+                  },
+                  cursorColor: Colors.black87,
+                  style: TextStyle(
+                    fontFamily: 'Varela',
+                    fontSize: 15.0,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: "댓글을 입력하세요...",
+                    hintStyle: TextStyle(
+                      fontFamily: 'Varela',
+                      color: Colors.black87.withOpacity(0.5),
+                    ),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
                   ),
                 ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.7,
-                  padding: EdgeInsets.only(left: 5.0, top: 5.0),
-                  height: 30.0,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[350],
-                    borderRadius: BorderRadius.circular(10),
+              ),
+            ],
+          ),
+        ),
+        ],
+      ),
+
+      //================================사진과 댓글내용 GET====================================
+      Row(
+        children: [
+          StreamBuilder<QuerySnapshot>( //이 안에 있는 데이타가 변경되야 스트림이 발동됨) ㅅㅂ
+            stream: _firestore
+                .collection('post')
+                .orderBy('time', descending: true)
+                .snapshots(),
+            //builder는 context와 리턴받을 것을 parameter로 가짐
+            builder: (context, snapshot) {
+              //스냅샷이 없는 경우 스피너
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.lightBlueAccent,
                   ),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          //textInputAction: TextInputAction.go, //엔터키 치면 제출되게 설정
-                          controller: messageTextController,
-                          onSubmitted: (value) {
-                            inputText = value;
-                            messageTextController.clear();
-                            _firestore.collection('post').add({
-                              //post라는 컬렉션에 contents컬럼으로 입력
-                              'contents': inputText,
-                              'time': DateFormat('yyyy-MM-dd kk:mm:ss')
-                                  .format(DateTime.now().toLocal()),
-                              'likey': likey,
-                            });
-                          },
-                          cursorColor: Colors.black87,
-                          style: TextStyle(
-                            fontFamily: 'Varela',
-                            fontSize: 15.0,
-                          ),
-                          decoration: InputDecoration(
-                            hintText: "댓글을 입력하세요...",
-                            hintStyle: TextStyle(
-                              fontFamily: 'Varela',
-                              color: Colors.black87.withOpacity(0.5),
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+                );
+              }
+              //messages는 post' 컬렉션 안에있는 데이터 내용
+              //snapshot에 있는 데이타 추출
+              final messages = snapshot.data!.docs;
+              // messageWidgets 컬럼형식의 리스트 선언
+               messageList=[];
+              for (var message in messages){
+                String messageText = message.get('contents');
+                String time = message.get('time');
+                DateTime? newMillennium = DateTime.tryParse(time);
+                String timesAgo = Jiffy(time).fromNow();
+                int likey = message.get('likey');
+                String docId = message.id;
+                MessageVO msgVO = new MessageVO(docId: '', messageText: '', time: '', timesAgo: '', likey: 0);
+                msgVO.messageText = messageText;
+                msgVO.time = time;
+                msgVO.timesAgo = timesAgo;
+                msgVO.likey = likey;
+                msgVO.docId = docId;
+                messageList.add(msgVO);
+              }
 
-            //================================사진과 댓글내용 GET====================================
-            Row(
-              children: [
-                StreamBuilder<QuerySnapshot>(
-                  stream: _firestore
-                      .collection('post')
-                      .orderBy('time', descending: true)
-                      .snapshots(),
-                  //builder는 context와 리턴받을 것을 parameter로 가짐
-                  builder: (context, snapshot) {
-                    //스냅샷이 없는 경우 스피너
-                    if (!snapshot.hasData) {
-                      return Center(
-                        child: CircularProgressIndicator(
-                          backgroundColor: Colors.lightBlueAccent,
-                        ),
-                      );
-                    }
-                    //messages는 post' 컬렉션 안에있는 데이터 내용
-                    //snapshot에 있는 데이타 추출
-                    final messages = snapshot.data!.docs;
-                    // messageWidgets 컬럼형식의 리스트 선언
+              return Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 1000.0,
+                      color: Colors.white,
+                      child: ListView.builder(
+                        itemCount: messageList.length,
+                        itemBuilder: (context, index) {
+                          if(messageList.length>0) {
 
+                          }
 
-                    for (var message in messages) {
-                      final messageText = message.get('contents');
-                      final time = message.get('time');
-                      DateTime? newMillennium = DateTime.tryParse(time);
-                      String timesAgo = Jiffy(time).fromNow();
-                      final likey = message.get('likey');
-                      final docId = message.id;
-                      MessageVO msgVO = new MessageVO(likey: 0,time: '',timesAgo: '',docId: '', messageText: '');
-
-                      msgVO.messageText = messageText;
-                      print(msgVO.messageText);
-                      msgVO.time = time;
-                      print(msgVO.time);
-                      msgVO.timesAgo = timesAgo;
-                      print(msgVO.timesAgo);
-                      msgVO.likey = likey;
-                      print(msgVO.likey);
-                      msgVO.docId = docId;
-                      print(msgVO.docId);
-                      messageList.add(msgVO);
-                    }
-
-                    return Expanded(
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 300,
-                            color: Colors.white,
-                              child: ListView.builder(
-                                itemCount: messageList.length,
-                                itemBuilder: (context, index) {
-                                  return Column(children: [
-                                    Row(children: [
-                                      CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                            "https://randomuser.me/api/portraits/men/28.jpg"),
-                                      ),
-                                      SizedBox(
-                                        width: 20.0,
-                                      ),
-                                      Container(
-                                        padding: EdgeInsets.all(5.0),
-                                        height: 30.0,
-                                        decoration: BoxDecoration(
-                                          color: Colors.grey[350],
-                                          borderRadius:
-                                          BorderRadius.circular(10),
-                                        ),
-                                        child: Text(
-                                            '${messageList[index].messageText}'),
-                                      ),
-                                    ]),
-                                    //bunch of reply
-                                    Row(
-                                      children: [
-                                        SizedBox(width: 75.0),
-                                        LikeButton(
-                                          onTap: onLikeButtonTapped,
-                                          size: 20.0,
-                                        ),
-                                        Text(
-                                          '${messageList[index].likey}',
-                                          style: TextStyle(
-                                            fontFamily: 'Varela',
-                                            fontSize: 10.0,
-                                          ),
-                                        ),
-                                        SizedBox(width: 10.0),
-                                        Text(
-                                          '${messageList[index].timesAgo}',
-                                          style: TextStyle(fontSize: 10.0),
-                                        ),
-                                        SizedBox(
-                                          width: 10.0,
-                                        ),
-                                        /*Text('답글달기',
+                          return Column(children: [
+                            Row(children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    "https://randomuser.me/api/portraits/men/28.jpg"),
+                              ),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(5.0),
+                                height: 30.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[350],
+                                  borderRadius:
+                                  BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                    '${messageList[index].messageText}'),
+                              ),
+                            ]),
+                            //bunch of reply
+                            Row(
+                              children: [
+                                SizedBox(width: 75.0),
+                                LikeButton(
+                                  onTap: onLikeButtonTapped,
+                                  size: 20.0,
+                                ),
+                                Text(
+                                  '${messageList[index].likey}',
+                                  style: TextStyle(
+                                    fontFamily: 'Varela',
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                Text(
+                                  '${messageList[index].timesAgo}',
+                                  style: TextStyle(fontSize: 10.0),
+                                ),
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                /*Text('답글달기',
                             style: TextStyle(
                             fontFamily: 'Varela',
                             fontSize: 14.0,
@@ -197,27 +200,26 @@ class _ReplyScreenState extends State<ReplyScreen> {
                           ),
                           Icon(Icons.mode_comment,
                           size: 15.0,), */
-                                      ],
-                                    ),
-                                  ]); //메시지위젯 -끝-
-                                },
-                              ),
+                              ],
+
                             ),
-                        ],
+                          ]); //메시지위젯 -끝-
+                        },
                       ),
-                    );
-                    //스크롤 이벤트 처리
-                  },
-
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            //좋아요와 댓글
+              );
 
-            //댓글을 입력하세요
-          ],
-        ),
+              //스크롤 이벤트 처리
+            },
+          ),
+        ],
       ),
+    //댓글을 입력하세요
+    ],
+    ),
+    ),
     );
   }
 
@@ -231,7 +233,7 @@ class _ReplyScreenState extends State<ReplyScreen> {
         .doc()
         .get()
         .then((DocumentSnapshot ds) {
-      print(["likey"]);
+
     });
 
     /// if failed, you can do nothing
@@ -240,7 +242,26 @@ class _ReplyScreenState extends State<ReplyScreen> {
     return !isLiked;
   }
 
+  mockFetch() async {
+    if (allLoaded) {
+      return;
+    }
+    setState(() {
+      loading = true;
 
+    });
+    await Future.delayed(Duration(milliseconds: 500));
+    List<MessageVO> newData = messageList.length >=10 ? []: List.generate(20,(index) => msgVO); //다음 20개 요소가 있는 새 목록을 반환
+    if(newData.isNotEmpty) {
+      messageList.addAll(newData);
+    }
+    setState(() {
+      loading = false;
+      allLoaded = newData.isEmpty;
+    });
+
+
+  }
 /*
   //좋아요
   void like() {
