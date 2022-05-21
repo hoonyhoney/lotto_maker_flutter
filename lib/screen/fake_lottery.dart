e9db0e968cc48b70baf4fe4b15895b4b343a3968
donghun <hoonyhoeny@gmail.com>
2022-05-21 23:11:41 +0900

    이미지 캡쳐 후 SHARE기능

--- lib/screen/fake_lottery.dart
+++ lib/screen/fake_lottery.dart
@@ -1,6 +1,19 @@
+
+import 'dart:io';
+
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui' as ui;
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
 import 'dart:math' as math;
 
 import '../services/win_numbers.dart';
@@ -16,7 +29,10 @@ class _FakeLotteryState extends State<FakeLottery> {
   String comment='';
    dynamic prize1;
    dynamic title;
-   ScreenshotController screenshotController = ScreenshotController(); //스크린샷 컨트롤러 객체 생성
+    int _counter = 0;
+    late Uint8List _imageFile= Uint8List(0); //imageFile초기화
+  //ScreenshotController screenshotController = ScreenshotController(); //스크린샷 컨트롤러 객체 생성
+  GlobalKey previewContainer = new GlobalKey();
 
 
    @override
@@ -33,6 +49,8 @@ class _FakeLotteryState extends State<FakeLottery> {
       title;
     });
   }
+
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<WinNumbers>(
@@ -52,15 +70,15 @@ class _FakeLotteryState extends State<FakeLottery> {
                 child: Column(
 
                   children: [
-                    Stack(
-                      children: [
+                    RepaintBoundary(
 
-                        Screenshot(
-                          controller: screenshotController,
-                          child: Container( //로또 이미지
+                      key: previewContainer,
+                      child: Stack(
+                        children: [
 
+                          Container( //로또 이미지
                             width:350,
-                            height:350,
+                            height:250,
                           decoration: BoxDecoration(
                               image: DecorationImage(
                                   image: AssetImage('images/fake_lottery.png'),
@@ -68,88 +86,109 @@ class _FakeLotteryState extends State<FakeLottery> {
                               )
                           ),
                           ),
-                        ),
-                        Positioned(
-                          top:100,
-                          left:185,
-                          child: Container(
-                            child: Transform.rotate(
-                              angle: -math.pi / 0,
-                              child: Text(
-                                prize1==null ? 'Loading':'$prize1',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color:Color(0xff5C4B30),//색깔은 0xff +
-                                  letterSpacing: 7.0,
+                          Positioned(
+                            top:50,
+                            left:185,
+                            child: Container(
+                              child: Transform.rotate(
+                                angle: -math.pi / 0,
+                                child: Text(
+                                  prize1==null ? 'Loading':'$prize1',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:Color(0xff5C4B30),//색깔은 0xff +
+                                    letterSpacing: 7.0,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        Positioned(
-                          top:123,
-                          left:180,
-                          child: Container(
-                            child: Text(
-                              title==null ? '':'$title'
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top:145,
-                          left:200,
-                          child: Container(
-                            child: Text('$comment',
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top:160,
-                          left:215,
-                          child: Container(
-                            child: Text('084578',
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: 1.0,
-                                color:Color(0xffDB9161),//색깔은 0xff +
+                          Positioned(
+                            top:73,
+                            left:180,
+                            child: Container(
+                              child: Text(
+                                title==null ? '':'$title'
                               ),
                             ),
                           ),
-                        ),
-                        Positioned(
-                          top:193,
-                          left:217,
-                          child: Container(
-                            child: Text('8',
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color:Color(0xff9F4257),//색깔은 0xff +
+                          Positioned(
+                            top:95,
+                            left:200,
+                            child: Container(
+                              child: Text('$comment',
                               ),
                             ),
                           ),
-                        ),
-                        Positioned(
-                          top:193,
-                          left:280,
-                          child: Container(
-                            child: Text('03',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 3.0,
-                                color:Color(0xff665C5B),//색깔은 0xff +
+                          Positioned(
+                            top:110,
+                            left:215,
+                            child: Container(
+                              child: Text('084578',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 1.0,
+                                  color:Color(0xffDB9161),//색깔은 0xff +
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                          Positioned(
+                            top:143,
+                            left:217,
+                            child: Container(
+                              child: Text('8',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  color:Color(0xff9F4257),//색깔은 0xff +
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top:143,
+                            left:280,
+                            child: Container(
+                              child: Text('03',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 3.0,
+                                  color:Color(0xff665C5B),//색깔은 0xff +
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                      RaisedButton(onPressed:(){
-                        screenshotController.capture(delay: Duration(milliseconds: 10)).
-                      }),
+                      RaisedButton(
+                        textColor: Colors.black87,
+                        color: Colors.amberAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular((10)),
+                        ),
+                        child: Wrap(
+                            alignment: WrapAlignment.center,
+                            children:
+                            [
+                              Icon(Icons.share,size: 18.0,
+                              ),
+                              SizedBox(width:5.0),
+                              Text(
+                                'Share to Friend ❤️‍' ,
+
+                              ),
+                            ]
+                        ),
+                        onPressed:(){ //Uint8List는 정수리스트라고 생각하면됨
+                        _captureSocialPng();
+
+                      },
+                      ),
 
                       Container(
                       padding: EdgeInsets.only(left:30.0,right: 30.0),
@@ -175,6 +214,31 @@ class _FakeLotteryState extends State<FakeLottery> {
       ),
     );
   }
+
+  Future<void> _captureSocialPng() {
+    List<String> imagePaths = [];
+    final RenderBox box = context.findRenderObject() as RenderBox;
+    return new Future.delayed(const Duration(milliseconds: 20), () async {
+      RenderRepaintBoundary? boundary = previewContainer.currentContext!
+          .findRenderObject() as RenderRepaintBoundary?;
+      ui.Image image = await boundary!.toImage();
+      final directory = (await getApplicationDocumentsDirectory()).path;
+      ByteData? byteData =
+      await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List pngBytes = byteData!.buffer.asUint8List();
+      File imgFile = new File('$directory/screenshot.png');
+      imagePaths.add(imgFile.path);
+      await imgFile.writeAsBytes(pngBytes).then((value) async {
+        await Share.shareFiles(imagePaths,
+            subject: 'Share',
+            text: 'Check this Out!',
+            sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+      }).catchError((onError) {
+        print(onError);
+      });
+    });
+  }
+
 
 }
 
